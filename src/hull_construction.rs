80f09b3c2a52ae03569b21f8c2ee9faade939cc2
Christoph Lürig <christoph.luerig@gmail.com>
2026-08-12 15:30:04 +0200

    first round of optimization

--- src/hull_construction.rs
+++ src/hull_construction.rs
@@ -5,11 +5,16 @@ use crate::{ConvexHullError, TriangleIndices};
 use fxhash::FxHashSet;
 use glam::Vec3;
 use rayon::prelude::{
-    IndexedParallelIterator, IntoParallelIterator, IntoParallelRefIterator, ParallelIterator,
+    IndexedParallelIterator, IntoParallelIterator, IntoParallelRefIterator,
+    IntoParallelRefMutIterator, ParallelIterator,
 };
+use slotmap::{DefaultKey, DenseSlotMap, Key};
 use std::mem::take;
 
 const RELATIVE_TOLERANCE: f32 = 1e-5;
+
+/// The triangle map used relatively often.
+type TriangleMap<'a> = DenseSlotMap<DefaultKey, Triangle<'a>>;
 
 /// Computes the numerical tolerance based on the bounding box diagonal
 pub(crate) fn compute_tolerance_value(vertices: &[Vec3]) -> f32 {
@@ -26,13 +31,60 @@ pub(crate) fn compute_tolerance_value(vertices: &[Vec3]) -> f32 {
     (max - min).length() * RELATIVE_TOLERANCE
 }
 
+/// This is a vertex we regard for processing.
+struct RegardedVertex {
+    /// The index into the original vertex array.
+    index: usize,
+    /// The furthest away triangle if known.
+    furthest_away_triangle: DefaultKey,
+    /// The distance to the triangle.
+    distance_to_furthest_triangle: f32,
+}
+
+impl RegardedVertex {
+    /// Constructs a new vertex from the index handed over.
+    fn new(index: usize) -> Self {
+        Self {
+            index,
+            furthest_away_triangle: DefaultKey::null(),
+            distance_to_furthest_triangle: f32::NEG_INFINITY,
+        }
+    }
+
+    /// Returns the distance to the triangle we are furthest away from, eventually updates the internal structure.
+    fn get_furthest_distance(&mut self, triangle_map: &TriangleMap) -> f32 {
+        if triangle_map.contains_key(self.furthest_away_triangle) {
+            self.distance_to_furthest_triangle
+        } else {
+            // The triangle is not in the map (anymore), so we have to search for it.
+            let (new_dist, new_triangle) = triangle_map.iter().fold(
+                (f32::NEG_INFINITY, DefaultKey::null()),
+                |(best_dist, best_key), (new_key, new_triangle)| {
+                    let dist = new_triangle.get_signed_distance(self.index);
+                    if dist > best_dist {
+                        (dist, new_key)
+                    } else {
+                        (best_dist, best_key)
+                    }
+                },
+            );
+
+            // Update the value.
+            self.distance_to_furthest_triangle = new_dist;
+            self.furthest_away_triangle = new_triangle;
+
+            new_dist
+        }
+    }
+}
+
 pub(crate) struct HullConstructor<'a> {
     /// The vertices we still need to process.
     vertices: &'a [Vec3],
     /// The hull as it currently stands in the construction process.
-    hull_triangles: Vec<Triangle<'a>>,
+    hull_triangles: TriangleMap<'a>,
     /// The list with the indices into the vertices that still have to be processed.
-    indices_to_process: Vec<usize>,
+    vertices_to_process: Vec<RegardedVertex>,
     /// The barrier we use for tolerance checks.
     tolerance: f32,
 }
@@ -40,11 +92,13 @@ pub(crate) struct HullConstructor<'a> {
 impl<'a> HullConstructor<'a> {
     /// Vertices are handed over.
     pub(crate) fn new(vertices: &'a [Vec3]) -> Self {
-        let indices_to_process = (0..vertices.len()).collect::<Vec<_>>();
+        let vertices_to_process = (0..vertices.len())
+            .map(|i| RegardedVertex::new(i))
+            .collect::<Vec<_>>();
         Self {
             vertices,
-            indices_to_process,
-            hull_triangles: Vec::new(),
+            vertices_to_process,
+            hull_triangles: TriangleMap::new(),
             tolerance: 1.0,
         }
     }
@@ -52,20 +106,37 @@ impl<'a> HullConstructor<'a> {
     /// Gets the best index pair as an index into the inner vector and the vertex position.
     fn get_best_index(&self, probe_function: impl Fn(usize) -> f32 + Sync) -> (usize, usize) {
         let (inner_index, result, _) = self
-            .indices_to_process
+            .vertices_to_process
             .par_iter()
             .enumerate()
-            .map(|(field_ind, vert_ind)| (field_ind, vert_ind, probe_function(*vert_ind)))
+            .map(|(field_ind, regarded_vertex)| {
+                (
+                    field_ind,
+                    regarded_vertex,
+                    probe_function(regarded_vertex.index),
+                )
+            })
             .max_by(|(_, _, val_a), (_, _, val_b)| val_a.total_cmp(val_b))
             .expect("There should be at least one vertex left");
-        (inner_index, *result)
+        (inner_index, result.index)
     }
     /// Applies the probe_function to all vertices and finds the element with the highest value,
     /// the index of that vertex is returned and eliminated from the processing list.
     fn get_best_index_and_remove(&mut self, probe_function: impl Fn(usize) -> f32 + Sync) -> usize {
         let (inner_index, result) = self.get_best_index(probe_function);
-        self.indices_to_process.swap_remove(inner_index);
+        self.vertices_to_process.swap_remove(inner_index);
         result
+    }
+
+    /// Cleans out all inner vertices by the contained data.
+    fn clean_inner(&mut self) {
+        self.vertices_to_process = take(&mut self.vertices_to_process)
+            .into_par_iter()
+            .filter_map(|mut vert| {
+                let dist = vert.get_furthest_distance(&self.hull_triangles);
+                (dist > self.tolerance).then_some(vert)
+            })
+            .collect();
     }
 
     /// Builds an initial tetrahedron from the inner vertices.
@@ -124,90 +195,99 @@ impl<'a> HullConstructor<'a> {
                 .collect();
         }
 
-        self.hull_triangles = triangles;
+        for tri in triangles {
+            self.hull_triangles.insert(tri);
+        }
         Ok(())
     }
 
     /// Analyzes the existing vertices and finds the index that is furthest away from the existing hull
-    /// and removes all vertices that are inside the hull. If there is no outer vertex left, it returns None.
-    fn get_best_vertex_index_and_sweep(&mut self) -> Option<usize> {
-        // Get highest signed distance for every vertex.
-        let highest_signed_distance = self
-            .indices_to_process
-            .par_iter()
-            .map(|i| {
-                self.hull_triangles
-                    .iter()
-                    .map(|tri| tri.get_signed_distance(*i))
-                    .fold(f32::NEG_INFINITY, f32::max)
-            })
-            .collect::<Vec<_>>();
-        // Get the position with the max value.
-        let (best_position, highest_value) = highest_signed_distance
-            .par_iter()
+    /// and removes all vertices that are inside the hull.
+    fn get_best_vertex_index_and_sweep(&mut self) -> usize {
+        let (best_index, highest_value) = self
+            .vertices_to_process
+            .par_iter_mut()
             .enumerate()
-            .max_by(|a, b| a.1.total_cmp(b.1))
-            .expect("indices_to_process is non-empty per loop condition");
+            .fold(
+                || (0usize, f32::NEG_INFINITY),
+                |(best_index, highest_value), (index, vertex)| {
+                    let dist = vertex.get_furthest_distance(&self.hull_triangles);
+                    if dist > highest_value {
+                        (index, dist)
+                    } else {
+                        (best_index, highest_value)
+                    }
+                },
+            )
+            .reduce(
+                || (0usize, f32::NEG_INFINITY),
+                |(best_index, highest_value), (new_index, new_value)| {
+                    if new_value > highest_value {
+                        (new_index, new_value)
+                    } else {
+                        (best_index, highest_value)
+                    }
+                },
+            );
 
-        // In this case we have only inner points left.
-        if *highest_value <= self.tolerance {
-            self.indices_to_process.clear();
-            return None;
-        }
-
-        let next_vertex = self.indices_to_process[best_position];
-        // Filter out the all vertices we do not need any more.
-        debug_assert_eq!(
-            self.indices_to_process.len(),
-            highest_signed_distance.len(),
-            "The two vectors should be of same length"
+        debug_assert!(
+            highest_value > 0.0,
+            "We should not get here with inner vertices."
         );
-        self.indices_to_process = self
-            .indices_to_process
-            .par_iter()
-            .zip(highest_signed_distance.into_par_iter())
-            .filter_map(|(&i, dist)| (i != next_vertex && dist > self.tolerance).then_some(i))
-            .collect();
 
-        Some(next_vertex)
+        let result = self.vertices_to_process[best_index].index;
+        self.vertices_to_process.swap_remove(best_index);
+
+        result
     }
 
     /// The inner call to generate a convex hull.
     pub(crate) fn generate_convex_hull(&mut self) -> Result<Vec<TriangleIndices>, ConvexHullError> {
         self.build_initial_tetrahedron()?;
-        while !self.indices_to_process.is_empty() {
+        self.clean_inner();
+        while !self.vertices_to_process.is_empty() {
             // Get the index the furthest away and remove all inner vertices along the way.
-            let Some(next_vertex) = self.get_best_vertex_index_and_sweep() else {
-                break;
-            };
+            let next_vertex = self.get_best_vertex_index_and_sweep();
 
-            // Partition the triangles in to be deleted and remaining.
-            let (mut remaining, deleted): (Vec<_>, Vec<_>) = take(&mut self.hull_triangles)
-                .into_iter()
-                .partition(|tri| tri.get_signed_distance(next_vertex) <= 0.0);
+            // Get the triangles to be deleted.
+            let deleted: Vec<_> = self
+                .hull_triangles
+                .iter()
+                .filter_map(|(key, tri)| {
+                    (tri.get_signed_distance(next_vertex) > 0.0).then(|| (key, tri.clone()))
+                })
+                .collect();
 
             // Extract the outer boundary edges of the elements that get deleted.
             let all_edges = deleted
-                .into_iter()
-                .flat_map(|tri| tri.edges())
+                .iter()
+                .flat_map(|(_, tri)| tri.edges())
                 .collect::<FxHashSet<_>>();
             let boundary_edges = all_edges
                 .iter()
                 .filter(|edge| !all_edges.contains(&edge.reversed()))
                 .collect::<Vec<_>>();
 
-            // Construct the kitting triangles from the boundary to the new vertex.
-            remaining.extend(
-                boundary_edges
-                    .iter()
-                    .map(|edge| Triangle::from_edge_and_points(self.vertices, edge, next_vertex)),
-            );
-            self.hull_triangles = remaining;
-        }
+            // Now we need to update the hull.
+            // First delete the old triangles.
+            for (index, _) in deleted {
+                self.hull_triangles.remove(index);
+            }
+            // Add the new ones.
+            for edge in boundary_edges {
+                self.hull_triangles.insert(Triangle::from_edge_and_points(
+                    self.vertices,
+                    edge,
+                    next_vertex,
+                ));
+            }
 
+            // Clean out.
+            self.clean_inner();
+        }
         Ok(self
             .hull_triangles
-            .iter()
+            .values()
             .map(|tri| tri.get_triple_representation())
             .collect())
     }
